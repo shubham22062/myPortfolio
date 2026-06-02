2988993fd2d09f0400c7201cc18bd5805fed3c5c
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-02 15:47:36 +0530

    changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
 
       <div className="flex gap-3 flex-wrap justify-center">
         <div className="pt-10">
-          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5">
+          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5 flex flex-col">
             <a
               href="https://dev.dg4uqajhampr9.amplifyapp.com/"
               target="_blank"
@@ -28,14 +28,14 @@ export default function Projects() {
               calls, chat, and integrated payments.
             </p>
 
-            <button className="border text-black border-gray-200 bg-gray-300 p-2 rounded-2xl transition-transform duration-300 hover:scale-105 mt-2">
+            <button className="mt-auto self-center px-6 py-2 bg-gray-300 text-black border border-gray-200 rounded-2xl font-medium transition-all duration-300 hover:scale-105 hover:bg-gray-400">
               View Project
             </button>
           </div>
         </div>
 
         <div className="pt-10">
-          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5">
+          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5 flex flex-col">
             <a
               href="https://finance-dashboard-system-9w37.vercel.app/"
               target="_blank"
@@ -51,10 +51,12 @@ export default function Projects() {
             <h2 className="font-bold py-6 text-xl">Finance Dashboard</h2>
 
             <p className="text-gray-600">
-              A full-stack financial management system built with React and Node.js that provides real-time spending insights through interactive charts and secure data tracking.
+              A full-stack financial management system built with React and
+              Node.js that provides real-time spending insights through
+              interactive charts and secure data tracking.
             </p>
 
-            <button className="border text-black border-gray-200 bg-gray-300 p-2 rounded-2xl transition-transform duration-300 hover:scale-105 mt-6">
+            <button className="mt-auto self-center px-6 py-2 bg-gray-300 text-black border border-gray-200 rounded-2xl font-medium transition-all duration-300 hover:scale-105 hover:bg-gray-400">
               View Project
             </button>
           </div>
@@ -63,35 +65,34 @@ export default function Projects() {
 
       <div className="flex gap-3 flex-wrap justify-center">
         <div className="pt-10">
-          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5">
+          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5 flex flex-col">
             <a
               href="https://dev.dg4uqajhampr9.amplifyapp.com/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
-                src="/Meshspire.svg"
-                alt="Meshspire"
+                src="/Awast.svg"
+                alt="AwastAi"
                 className="w-full sm:w-80 h-40 rounded-2xl transition-transform duration-300 hover:scale-105"
               />
             </a>
 
-            <h2 className="font-bold py-6 text-xl">MeshSpire</h2>
+            <h2 className="font-bold py-6 text-xl">AwastAi</h2>
 
             <p className="text-gray-600">
-              A real-time tutoring platform built with Next.js and WebRTC that
-              facilitates seamless student-tutor interaction through live video
-              calls, chat, and integrated payments.
+             Developed a full-stack AI chatbot that leverages large language models to answer questions,
+              assist users, and provide personalized interactions..
             </p>
 
-            <button className="border text-black border-gray-200 bg-gray-300 p-2 rounded-2xl transition-transform duration-300 hover:scale-105 mt-2">
+            <button className="mt-auto self-center px-6 py-2 bg-gray-300 text-black border border-gray-200 rounded-2xl font-medium transition-all duration-300 hover:scale-105 hover:bg-gray-400">
               View Project
             </button>
           </div>
         </div>
 
         <div className="pt-10">
-          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5">
+          <div className="border border-gray-200 rounded-xl w-[90vw] sm:w-[360px] h-auto sm:h-[440px] p-5 flex flex-col">
             <a
               href="https://finance-dashboard-system-9w37.vercel.app/"
               target="_blank"
@@ -107,10 +108,12 @@ export default function Projects() {
             <h2 className="font-bold py-6 text-xl">Finance Dashboard</h2>
 
             <p className="text-gray-600">
-              A full-stack financial management system built with React and Node.js that provides real-time spending insights through interactive charts and secure data tracking.
+              A full-stack financial management system built with React and
+              Node.js that provides real-time spending insights through
+              interactive charts and secure data tracking.
             </p>
 
-            <button className="border text-black border-gray-200 bg-gray-300 p-2 rounded-2xl transition-transform duration-300 hover:scale-105 mt-5">
+            <button className="mt-auto self-center px-6 py-2 bg-gray-300 text-black border border-gray-200 rounded-2xl font-medium transition-all duration-300 hover:scale-105 hover:bg-gray-400">
               View Project
             </button>
           </div>
